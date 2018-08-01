0ed3a703b7cf269bd7df485d7c955781b540cab0
Akshay Khole <akshay@reversehack.in>
2018-07-31 22:07:11 -0700

    More fixes

--- main.py
+++ main.py
@@ -111,7 +111,6 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
         pool3_out_scaled,
         num_classes,
         1,
-        strides=(1,1),
         padding = 'same',
         kernel_initializer = tf.random_normal_initializer(stddev = 0.01),
         kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3)
